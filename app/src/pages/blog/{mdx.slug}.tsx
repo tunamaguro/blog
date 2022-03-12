f54f5f0cd7b8438e6131365cb27f1875c366b237
tunamaguro <tsu7magu6@gmail.com>
2022-03-12 18:32:39 +0900

    Finished Gatsby Tutotial part7

--- app/src/pages/blog/{mdx.slug}.tsx
+++ app/src/pages/blog/{mdx.slug}.tsx
@@ -1,11 +1,21 @@
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
 import Layout from "../../components/layout";
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -16,8 +26,16 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       body
       frontmatter {
-        date(formatString: "MMMM D, YYYY")
         title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
