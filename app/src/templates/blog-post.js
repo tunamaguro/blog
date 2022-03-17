e8e24a3d586140e4e12e3692bbb807afe395b6d2
tunamaguro <tsu7magu6@gmail.com>
2022-03-17 23:48:04 +0900

    change blog-post design

--- app/src/templates/blog-post.js
+++ app/src/templates/blog-post.js
@@ -1,7 +1,30 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
-const BlogPostTemplate = () => {
-  return <h1>Blog Template</h1>
+import PageBase from "../components/PageBase"
+
+const BlogPostTemplate = ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <PageBase>
+      <section
+        className="prose lg:prose-lg"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </PageBase>
+  )
 }
 
 export default BlogPostTemplate
+
+export const query = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        date(formatString: "YYYY/MM/DD")
+        title
+      }
+      html
+    }
+  }
+`
