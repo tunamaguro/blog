79982db840cfa942e04a67d9152abacaeb195731
tunamaguro <tsu7magu6@gmail.com>
2022-12-25 08:34:44 +0000

    article page

--- app/src/pages/articles/{mdx.frontmatter__slug}.tsx
+++ app/src/pages/articles/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { PageProps, HeadFC, graphql } from "gatsby";
 import Seo from "../../components/Seo";
+import { Layout } from "../../components/Layout";
+import { Twemoji } from "../../components/Twemoji";
 
-const BlogPost: React.FC<PageProps> = ({ pageContext, data, children }) => {
-  console.log(pageContext);
-  return <div>{children}</div>;
+const BlogPost: React.FC<PageProps> = (props) => {
+  const { pageContext, data, children } = props;
+  console.log(props);
+  return (
+    <Layout>
+      <div className="container max-w-5xl mx-auto py-4">
+        <div className="flex flex-col items-center gap-2 pb-8">
+          <span className="w-24">
+            <Twemoji emoii={data.mdx.frontmatter.emoji} />
+          </span>
+          <h1 className="text-4xl font-bold text-primary-content">
+            {data.mdx.frontmatter.title}
+          </h1>
+          <p className="">createdAt : {data.mdx.frontmatter.date}</p>
+          <div className="grid grid-flow-col gap-4">
+            {data.mdx.frontmatter.tags.map((tag) => (
+              <div className="badge badge-outline">{tag}</div>
+            ))}
+          </div>
+        </div>
+        <div className="bg-base-200 p-8 rounded-3xl">
+          <div className="prose max-w-none">{children}</div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export const query = graphql`
@@ -12,7 +37,9 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "yyyy-MM-DD")
+        emoji
+        tags
       }
     }
   }
