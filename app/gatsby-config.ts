f988499e2d747115f3f00753328d0cdd0a625473
tunamaguro <tsu7magu6@gmail.com>
2022-12-25 21:55:50 +0000

    fix siteURL

--- app/gatsby-config.ts
+++ app/gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `tunamaguro's blog`,
     author: `tunamaguro`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://tunamaguro.dev/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
