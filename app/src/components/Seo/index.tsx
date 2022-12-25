4160c7590453c56c014478ec37c16be000489829
tunamaguro <tsu7magu6@gmail.com>
2022-12-25 06:35:50 +0000

    change query name

--- app/src/components/Seo/index.tsx
+++ app/src/components/Seo/index.tsx
@@ -3,7 +3,7 @@ import { graphql, useStaticQuery } from "gatsby";
 
 const Seo = ({ title = "" }) => {
   const data = useStaticQuery(graphql`
-    query MyQuery {
+    query TitleQuery {
       site {
         siteMetadata {
           title
