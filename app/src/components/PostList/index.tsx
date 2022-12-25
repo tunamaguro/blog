f1e76460665b32cc9820f6f1060c1fc6d5f4abb9
tunamaguro <tsu7magu6@gmail.com>
2022-12-25 06:43:09 +0000

    Layout

--- app/src/components/PostList/index.tsx
+++ app/src/components/PostList/index.tsx
@@ -22,7 +22,7 @@ export const PostList: React.FC = () => {
 
   const nodes = data.allMdx.nodes;
   return (
-    <div className="container mx-auto flex-grow">
+    <div className="container mx-auto">
       <h1 className="text-3xl font-bold border-b-4 border-base-content py-4">
         Blog
       </h1>
