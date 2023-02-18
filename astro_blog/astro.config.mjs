dce09ec0216715949ca7fa5f21ec6d8fc4792ff3
tunamaguro <tsu7magu6@gmail.com>
2023-02-18 14:23:04 +0000

    prettier

--- astro_blog/astro.config.mjs
+++ astro_blog/astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact(), tailwind()]
+  integrations: [mdx(), preact(), tailwind()],
 });
