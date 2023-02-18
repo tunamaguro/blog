4a7117e66b259b040b7be2ffb75f4c610ee0f775
tunamaguro <tsu7magu6@gmail.com>
2023-02-18 14:21:11 +0000

    とりあえずスタイル関係

--- astro_blog/astro.config.mjs
+++ astro_blog/astro.config.mjs
@@ -3,6 +3,9 @@ import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact()],
+  integrations: [mdx(), preact(), tailwind()]
 });
