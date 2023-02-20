dd0e2cd3a9e6b4f79d8d7ae726f3206d14ced1f0
tunamaguro <tsu7magu6@gmail.com>
2023-02-20 09:49:23 +0000

    Content

---
+++ astro_blog/src/content/config.ts
@@ -0,0 +1,16 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const ArticleCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    emoji: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  article: ArticleCollection,
+};
