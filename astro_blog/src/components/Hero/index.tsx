1e92d5b1c7141a417622b85ce5bf9e7842590ed9
tunamaguro <tsu7magu6@gmail.com>
2023-02-20 12:22:47 +0000

    fix

--- astro_blog/src/components/Hero/index.tsx
+++ astro_blog/src/components/Hero/index.tsx
@@ -17,11 +17,11 @@ export const Hero: FunctionComponent<Props> = ({ author }) => (
     </h1>
     <div className="flex py-4 gap-8">
       <div className="avatar">
-        <div className="w-40 p-3 rounded-full bg-base-300">
+        <div className="w-40 p-3 rounded-full bg-base-300 ">
           <img
             alt="tsunamaguro icon"
-            src="/icon.png"
-            className="transform rotate-45 translate-y-4"
+            src="/maguro.png"
+            className="transform rotate-45 translate-y-4 !h-24"
           />
         </div>
       </div>
