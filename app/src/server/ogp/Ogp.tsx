6e19380a28d5ad4d444b14f2b7262decd3237d8d
tunamaguro <tsu7magu6@gmail.com>
2023-05-05 15:31:52 +0000

    配置を見直し

--- app/src/server/ogp/Ogp.tsx
+++ app/src/server/ogp/Ogp.tsx
@@ -1,67 +1,66 @@
-export function Ogp({
-  title,
-  iconImage,
-}: {
-  title: string;
-  iconImage: string;
-}) {
+import type { PropsWithChildren } from "react";
+
+/**
+ * GitHub user avatar image
+ */
+const iconImage = "https://avatars.githubusercontent.com/u/79092292?v=4";
+
+export function Ogp({ title }: { title: string }) {
   return (
     <div
       style={{
-        height: "100%",
-        width: "100%",
         display: "flex",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        backgroundColor: "rgb(42 48 60)",
-        padding: 48,
-        border: "48px solid rgb(32 37 46)",
-        color: "rgb(255 255 255)",
+        width: "100%",
+        height: "100%",
+        padding: "48px",
+        backgroundColor: "rgb(32 37 46)",
       }}
     >
       <div
         style={{
-          fontSize: "48px",
-          fontWeight: "700",
-        }}
-      >
-        {title}
-      </div>
-      <div
-        style={{
           display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-          color: "rgb(166 173 187)",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgb(42 48 60)",
+          padding: "36px",
+          borderRadius: "24px",
+          color: "rgb(255 255 255)",
         }}
       >
         <div
           style={{
+            fontSize: "60px",
+            fontWeight: "700",
+          }}
+        >
+          {title}
+        </div>
+        <div
+          style={{
             display: "flex",
-            justifyContent: "center",
+            justifyContent: "flex-end",
             alignItems: "center",
-            width: "36px",
-            height: "36px",
-            borderRadius: "9999px",
-            padding: "4px",
-            marginRight: "12px",
-
-            backgroundColor: "rgb(32 37 46)",
+            gap: "12px",
             fontSize: "36px",
           }}
         >
-          <img
-            src={iconImage}
-            width={36}
-            height={36}
-            style={{
-              objectFit: "contain",
-              transform: "rotate(45deg)",
-            }}
-          />
+          <img src={iconImage} width={36} height={36} />
+          <div>tunamaguro's blog</div>
         </div>
-        <span>tunamaguro's blog</span>
       </div>
     </div>
   );
 }
+
+export const Wrapper = ({ children }: PropsWithChildren) => (
+  <div
+    style={{
+      width: "800px",
+      height: "400px",
+    }}
+  >
+    {children}
+  </div>
+);
