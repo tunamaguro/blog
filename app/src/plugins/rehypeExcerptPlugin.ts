69ed7293ebab130d94c051bdd635442427f3972c
tunamaguro <tsu7magu6@gmail.com>
2023-05-15 14:14:38 +0000

    ellipsis

--- app/src/plugins/rehypeExcerptPlugin.ts
+++ app/src/plugins/rehypeExcerptPlugin.ts
@@ -3,13 +3,13 @@ import { truncate, type Options } from "hast-util-truncate";
 
 /** @type {import('unified').Plugin<[Options]>} */
 export function rehypeExcerptContent(
-  options: Options
+  options: Options = { ellipsis: "…" }
 ):
   | void
   | import("unified").Transformer<import("hast").Root, import("hast").Root> {
   return function (tree, { data }) {
-    const truncatedTree = truncate(tree, options);
-    const excerpt = toString(truncatedTree).replaceAll(/\s/g, "");
+    const truncatedTree = truncate(tree, { ellipsis: '…', ...options });
+    const excerpt = toString(truncatedTree).replaceAll(/\s/g, " ");
     // @ts-ignore See https://docs.astro.build/en/guides/markdown-content/#modifying-frontmatter-programmatically
     data.astro.frontmatter.excerpt = excerpt;
   };
