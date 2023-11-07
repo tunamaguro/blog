6275e13199f6db844e0a3ab6c0f5c5faa0a1b18c
tunamaguro <tsu7magu6@gmail.com>
2023-11-07 12:52:40 +0000

    feat: add Misskey link

---
+++ app/src/components/Hero/MisskeyIcon.tsx
@@ -0,0 +1,5 @@
+import misskeyIcon from "./misskey-icon.svg"
+
+type Props = JSX.IntrinsicElements["img"]
+
+export const MisskeyIcon = (props: Props) => <img src={misskeyIcon.src} alt="Misskey" {...props} />
