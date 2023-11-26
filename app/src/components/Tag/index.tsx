c2cf457d8530eeba616685d8f6ad9fad758c4bdf
tunamaguro <tsu7magu6@gmail.com>
2023-11-26 13:20:25 +0000

    feat: ダサいホバーアニメーションを追加

--- app/src/components/Tag/index.tsx
+++ app/src/components/Tag/index.tsx
@@ -6,7 +6,7 @@ type TagProps = { tag: string, children: ReactNode };
 
 export const Tag = ({ tag, children }: TagProps) => {
     return <a href={`/tags/${slugfy(tag)}`}>
-        <div className="badge badge-outline">
+        <div className="badge badge-outline hover:bg-primary hover:text-primary-content">
             {children}
         </div>
     </a>
