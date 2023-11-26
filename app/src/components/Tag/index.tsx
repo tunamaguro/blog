f6f59a2e089d2e373fb344d305a043cfce8fbb8b
tunamaguro <tsu7magu6@gmail.com>
2023-11-26 14:14:36 +0000

    fix

--- app/src/components/Tag/index.tsx
+++ app/src/components/Tag/index.tsx
@@ -1,14 +1,14 @@
 import type { ReactNode } from "react";
-import { slugfy } from "@/utils/slugfy"
+import { slugfy } from "@/utils/slugfy";
 
-type TagProps = { tag: string, children: ReactNode };
-
+type TagProps = { tag: string; children: ReactNode };
 
 export const Tag = ({ tag, children }: TagProps) => {
-    return <a href={`/tags/${slugfy(tag)}`}>
-        <div className="badge badge-outline hover:bg-primary hover:text-primary-content">
-            {children}
-        </div>
+  return (
+    <a href={`/tags/${slugfy(tag)}`}>
+      <div className="badge badge-outline hover:bg-primary hover:text-primary-content">
+        {children}
+      </div>
     </a>
-
-}
+  );
+};
