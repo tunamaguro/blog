0dc63e29e31c7c2e4f0f8bb068fccba90e37b463
tunamaguro <tsu7magu6@gmail.com>
2024-01-18 14:25:23 +0000

    refactor: aタグの当たり判定を修正
    qiitaの実装を参考にした

--- app/src/components/Tag/index.tsx
+++ app/src/components/Tag/index.tsx
@@ -5,10 +5,10 @@ type TagProps = { tag: string; children: ReactNode };
 
 export const Tag = ({ tag, children }: TagProps) => {
   return (
-    <a href={`/tags/${slugfy(tag)}`}>
-      <div className="badge badge-outline hover:bg-primary hover:text-primary-content whitespace-nowrap">
+    <li className="badge badge-outline hover:bg-primary hover:text-primary-content whitespace-nowrap">
+      <a href={`/tags/${slugfy(tag)}`}>
         {children}
-      </div>
-    </a>
+      </a>
+    </li>
   );
 };
