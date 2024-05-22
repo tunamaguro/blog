bd5fd6471d8b0f094512c7b291ed7c41fc7544af
tunamaguro <tsu7magu6@gmail.com>
2024-05-22 13:16:58 +0000

    style: 不要なパディングを削除

--- app/src/components/Footer/index.tsx
+++ app/src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 export const Footer: FunctionComponent<Props> = ({ author }) => (
-  <footer className="footer footer-center p-10 bg-neutral text-neutral-content rounded">
+  <footer className="footer footer-center py-10 bg-neutral text-neutral-content text-base">
     <div className="grid grid-flow-col gap-4">
       <a href="/" className="link link-hover">
         Home
