955f2c1af881e26573df3e1764d42ece79dd3966
tunamaguro <tsu7magu6@gmail.com>
2024-05-22 12:04:01 +0000

    wip: pagefind

---
+++ app/src/components/PostSerch/pagefind-ui.d.ts
@@ -0,0 +1,21 @@
+declare module "@pagefind/default-ui" {
+    declare class PagefindUI {
+        constructor(opts: {
+            element?: string | HTMLElement,
+            bundlePath?: string,
+            pageSize?: number,
+            resetStyles?: boolean,
+            showImages?: boolean,
+            showSubResults?: boolean,
+            excerptLength?: number,
+            processResult?: any,
+            processTerm?: any,
+            showEmptyFilters?: boolean,
+            debounceTimeoutMs?: number,
+            mergeIndex?: any,
+            translations?: any,
+            autofocus?: boolean,
+            sort?: any,
+        })
+    }
+}
