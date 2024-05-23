353e41b2063c6d425048cf5b550a4526e55906e5
tunamaguro <tsu7magu6@gmail.com>
2024-05-23 19:37:19 +0000

    chore: prettier

--- app/src/components/SearchDialog/SearchDialog.tsx
+++ app/src/components/SearchDialog/SearchDialog.tsx
@@ -1,20 +1,19 @@
 import type { ReactNode } from "react";
 
-
 export type Props = {
-    dialog_id: string;
-    children?: ReactNode
+  dialog_id: string;
+  children?: ReactNode;
 };
 
 export const SearchDialog = ({ dialog_id, children }: Props) => {
-    return (
-        <dialog id={dialog_id} className="modal items-start">
-            <div className="modal-box mt-24 max-w-5xl min-h-64 mx-auto">
-                {children}
-            </div>
-            <form method="dialog" className="modal-backdrop">
-                <button>close</button>
-            </form>
-        </dialog>
-    );
+  return (
+    <dialog id={dialog_id} className="modal items-start">
+      <div className="modal-box mt-24 max-w-5xl min-h-64 mx-auto">
+        {children}
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
 };
