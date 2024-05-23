58e01cfade1166b47afadf203ca92a5fe87a92d9
tunamaguro <tsu7magu6@gmail.com>
2024-05-23 18:58:52 +0000

    feat: 検索ダイアログの実装

--- app/src/components/SearchDialog/SearchDialog.tsx
+++ app/src/components/SearchDialog/SearchDialog.tsx
@@ -1,13 +1,16 @@
+import type { ReactNode } from "react";
+
+
 export type Props = {
     dialog_id: string;
+    children?: ReactNode
 };
 
-export const SearchDialog = ({ dialog_id }: Props) => {
+export const SearchDialog = ({ dialog_id, children }: Props) => {
     return (
-        <dialog id={dialog_id} className="modal modal-top" open>
-            <div className="modal-box modal-top">
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click outside to close</p>
+        <dialog id={dialog_id} className="modal items-start">
+            <div className="modal-box mt-24 max-w-5xl min-h-64 mx-auto">
+                {children}
             </div>
             <form method="dialog" className="modal-backdrop">
                 <button>close</button>
