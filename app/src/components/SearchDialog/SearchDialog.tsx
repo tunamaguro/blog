1bdf2a4a9390d67d2509c0a2314ddfa32e0d8154
tunamaguro <tsu7magu6@gmail.com>
2024-05-23 20:16:04 +0000

    style: invisibleを明記

--- app/src/components/SearchDialog/SearchDialog.tsx
+++ app/src/components/SearchDialog/SearchDialog.tsx
@@ -1,13 +1,15 @@
-import type { ReactNode } from "react";
+import type { DialogHTMLAttributes, ReactNode } from "react";
+
+import { clsx } from "clsx";
 
 export type Props = {
   dialog_id: string;
   children?: ReactNode;
-};
+} & DialogHTMLAttributes<HTMLDialogElement>;
 
-export const SearchDialog = ({ dialog_id, children }: Props) => {
+export const SearchDialog = ({ dialog_id, children, className, ...ret }: Props) => {
   return (
-    <dialog id={dialog_id} className="modal items-start">
+    <dialog id={dialog_id} className={clsx("modal items-start", className)} {...ret}>
       <div className="modal-box mt-24 max-w-5xl min-h-64 mx-auto">
         {children}
       </div>
