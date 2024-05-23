b3190ee250f036af25df2b85a4baec6086440a5b
tunamaguro <tsu7magu6@gmail.com>
2024-05-23 18:06:26 +0000

    style: カーソルを合わせた際の挙動を変更

--- app/src/components/SearchDialog/SearchBar.tsx
+++ app/src/components/SearchDialog/SearchBar.tsx
@@ -2,15 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 export const SearchBar = () => {
     return (
-        <div className="w-full text-lg bg-base-200 rounded-md flex cursor-text focus:outline items-center m-1">
-            <span className="py-6 px-4">
-                <FontAwesomeIcon className="h-4 w-4 text-base-content" strokeWidth={0.5} icon={faMagnifyingGlass} />
-            </span>
-            <div className="grid items-center justify-center font-bold text-xl text-base-content focus:outline-none">
-                <span>
-                    検索
+        <div className="p-1">
+            <button className="w-full text-lg bg-base-200 rounded-md flex cursor-pointer hover:outline hover:font-bold items-center">
+                <span className="py-6 px-4">
+                    <FontAwesomeIcon className="h-4 w-4 text-base-content" strokeWidth={0.5} icon={faMagnifyingGlass} />
                 </span>
-            </div>
+                <div className="grid items-center justify-center text-xl text-base-content focus:outline-none">
+                    <span>
+                        検索
+                    </span>
+                </div>
+            </button>
         </div>
+
     );
 };
