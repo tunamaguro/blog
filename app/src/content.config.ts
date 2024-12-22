4f6fc86d37f19a10cd0bcda779054379f0a2f3c8
tunamaguro <tsu7magu6@gmail.com>
2024-12-22 05:26:28 +0000

    chore: pnpm format

--- app/src/content.config.ts
+++ app/src/content.config.ts
@@ -5,17 +5,20 @@ import { glob } from "astro/loaders";
 const categories = ["blog", "tech"] as const;
 
 const article = defineCollection({
-    loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./src/content/article" }),
-    schema: z.object({
-        draft: z.boolean().default(false),
-        title: z.string(),
-        category: z.enum(categories),
-        createdAt: z.string(),
-        updatedAt: z.string().optional(),
-        emoji: z.string(),
-        tags: z.array(z.string()).optional().default([]),
-        description: z.string().optional(),
-    }),
+  loader: glob({
+    pattern: ["**/*.md", "**/*.mdx"],
+    base: "./src/content/article",
+  }),
+  schema: z.object({
+    draft: z.boolean().default(false),
+    title: z.string(),
+    category: z.enum(categories),
+    createdAt: z.string(),
+    updatedAt: z.string().optional(),
+    emoji: z.string(),
+    tags: z.array(z.string()).optional().default([]),
+    description: z.string().optional(),
+  }),
 });
 
 export const collections = { article };
