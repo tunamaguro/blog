e217a4f90047e6772ecd2455c2bb6bf5f3f49046
tunamaguro <contact@tunamaguro.dev>
2025-03-18 15:06:24 +0000

    refactor: replace to vanilla-extract

---
+++ app/src/components/PostSearch/pagefind.css.ts
@@ -0,0 +1,19 @@
+// @ts-nocheck allow specific var name
+import { colors } from "src/styles/theme.css"
+import { style, } from "@vanilla-extract/css"
+
+
+export const uiOverride = style({
+    "--pagefind-ui-scale": "1",
+    "--pagefind-ui-primary": colors.color.primary,
+    "--pagefind-ui-text": colors.surface.baseContent,
+    "--pagefind-ui-background": colors.surface.base300,
+    "--pagefind-ui-border": colors.surface.base200,
+    "--pagefind-ui-tag": colors.surface.base200,
+    "--pagefind-ui-border-width": "2px",
+    "--pagefind-ui-border-radius": "8px",
+    "--pagefind-ui-image-border-radius": "8px",
+    "--pagefind-ui-image-box-ratio": "3 / 2",
+    "--pagefind-ui-font":
+        "Hiragino Kaku Gothic ProN, Hiragino Sans, Meiryo, Arial, sans-serif",
+})
