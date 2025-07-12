2a57dbdca43677c858dcacae5f918925d5957d57
tunamaguro <contact@tunamaguro.dev>
2025-07-12 07:59:56 +0000

    feat: change path

--- app/src/pages/ogp/[...slug].png.ts
+++ app/src/pages/ogp/[...slug].png.ts
@@ -1,12 +1,12 @@
 import type { APIRoute, InferGetStaticPropsType } from "astro";
 import { generateOgp } from "@/server/ogp/generateOgp";
-import { getArticles } from "@/utils/getArticles";
+import { getArticles, articleToPath } from "@/utils/getArticles";
 
 export const getStaticPaths = async () => {
   const articles = await getArticles();
 
   return articles.map((article) => ({
-    params: { slug: article.data.slug },
+    params: { slug: articleToPath(article) },
     props: {
       data: article.data,
     },
--- app/src/utils/getArticles.ts
+++ app/src/utils/getArticles.ts
@@ -1,4 +1,4 @@
-import { getCollection } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 
 export const getArticles = () => {
   return getCollection("article", (article) => !article.data.draft).then(
@@ -6,3 +6,18 @@ export const getArticles = () => {
       articles.sort((a, b) => (a.data.createdAt < b.data.createdAt ? 1 : -1)),
   );
 };
+
+
+const lastComponent = (s: string) => {
+  return s.split("/").at(-1);
+};
+
+export const articleToPath = (article: CollectionEntry<"article">): string => {
+  const createdAt = new Date(article.data.createdAt);
+  const y = createdAt.getFullYear();
+  const m = String(createdAt.getMonth() + 1).padStart(2, "0");
+
+  const parts = lastComponent(article.id);
+
+  return `${y}/${m}/${parts}`;
+}
